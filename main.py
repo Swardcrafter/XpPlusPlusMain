f8386ec5284a8b328deef0d255e67d9af7349460
PlotTwist <84284643+Swardcrafter@users.noreply.github.com>
2023-10-31 20:12:11 -0700

    1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.config["SECRET_KEY"] = os.environ['SECRET_KEY']
 
 @app.route("/")
 def home():
-    return render_template("index.html", discord_url=os.environ['LOGIN_URL'])
+    return render_template("home_page/index.html", discord_url=os.environ['LOGIN_URL'])
 
 
 @app.route("/login")
@@ -21,7 +21,7 @@ def login():
 	user = Oauth.get_user_json(at)
 	user_name, user_id = user.get("username"), user.get("discriminator")
 	
-	return f"Success, logged in as {user_name}#{user_id}"
+	return render_template("logged_page/index.html", user_name=user_name, user_id=user_id)
 
 
 if(__name__ == "__main__"):
