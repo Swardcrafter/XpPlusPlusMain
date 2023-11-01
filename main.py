a5c75384b986d4efb6a9d6069b63617a3df6bb09
Swardcrafter <84284643+Swardcrafter@users.noreply.github.com>
2023-11-01 02:33:42 +0000

    got user login verification

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+from oauth import Oauth
 import os
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ['SECRET_KEY']
 
-login_url = os.environ['LOGIN_URL']
 
 @app.route("/")
 def home():
-    return render_template("index.html", discord_url=login_url)
+    return render_template("index.html", discord_url=os.environ['LOGIN_URL'])
 
 
 @app.route("/login")
 def login():
-	return "Done"
+	code = request.args.get("code")
+
+	at = Oauth.get_access_token(code)
+	session["token"] = at
+
+	user = Oauth.get_user_json(at)
+	user_name, user_id = user.get("username"), user.get("discriminator")
+	
+	return f"Success, logged in as {user_name}#{user_id}"
 
 
 if(__name__ == "__main__"):
