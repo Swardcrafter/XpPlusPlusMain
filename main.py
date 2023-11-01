ef950d9468fd78e0e8affa87fd38974bcd2b42e6
PlotTwist <84284643+Swardcrafter@users.noreply.github.com>
2023-10-31 19:08:01 -0700

    1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", discord_url="https://www.github.com/")
 
 
 if(__name__ == "__main__"):
