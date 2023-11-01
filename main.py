faba685f22b52367bb96de871fd09405e4e2ed3c
Swardcrafter <84284643+Swardcrafter@users.noreply.github.com>
2023-11-01 02:21:34 +0000

    1

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+login_url = os.environ['LOGIN_URL']
 
 @app.route("/")
 def home():
-    return render_template("index.html", discord_url="https://www.github.com/")
+    return render_template("index.html", discord_url=login_url)
+
+
+@app.route("/login")
+def login():
+	return "Done"
 
 
 if(__name__ == "__main__"):
