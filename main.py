886d6d75ad3de35321990edaf299887991422694
PlotTwist <84284643+Swardcrafter@users.noreply.github.com>
2023-10-31 19:06:19 -0700

    1

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return 'Hello from Flask!'
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
-app.run(host='0.0.0.0', port=81)
+if(__name__ == "__main__"):
+    app.run(host='0.0.0.0', port=81)
